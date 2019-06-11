5a50c0684fb445e664eaf078720b3fcfd9f5bef6
arijdb <ariane.jurado@gmail.com>
2019-06-11 22:49:07 +0200

    linked js with scss

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    
+  }
+  render() {
+    return (
+      <h1>Mi listado de pelis</h1>
+    );
+  }
+}
+
+export default App;
