f6e303ed1fad571275d9bf86c9feb3f7064f77d0
arijdb <ariane.jurado@gmail.com>
2019-06-14 10:19:44 +0200

    publish in github pages

---
+++ docs/build/precache-manifest.55806e819c0e0c93f8524c0e21a14e49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a482b2a49c871e769f1dce49ae1384",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7024cc244396325169c9",
+    "url": "./static/css/main.d0302070.chunk.css"
+  },
+  {
+    "revision": "1b4c6c67ad0802bec18f",
+    "url": "./static/js/2.563148e4.chunk.js"
+  },
+  {
+    "revision": "7024cc244396325169c9",
+    "url": "./static/js/main.b16e30b8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
