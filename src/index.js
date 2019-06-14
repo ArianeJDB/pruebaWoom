036827c3b909b96b8e789063d4ab1363e7481c23
arijdb <ariane.jurado@gmail.com>
2019-06-14 10:14:55 +0200

    trying service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {HashRouter} from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 import './index.scss';
 
 ReactDOM.render(
@@ -10,3 +11,4 @@ ReactDOM.render(
   </HashRouter>, 
   document.getElementById('root'));
   
+  serviceWorker.register();
